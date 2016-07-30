3b210e9063450b81fae2705dd0bcd3fac72afa25
Liang Chen <chen348@indiana.edu>
2016-07-30 19:38:39 -0400

    update score parsing

--- scoreParsingBatch.py
+++ scoreParsingBatch.py
@@ -1,4 +1,5 @@
 
+from __future__ import print_function
 import numpy as np
 from scoreParser import scoreParser
 from globalv import rhythm_label_num, pitch_max_num
@@ -19,12 +20,13 @@ for i in xrange(3000):
             line_rhythm = frhythm.readline()
             pitch_prob = np.vstack((pitch_prob, np.array([float(f) for f in line_pitch.strip().split()])))
             rhythm_prob = np.vstack((rhythm_prob, np.array([float(f) for f in line_rhythm.strip().split()])))
-        if i < 4:
-            continue
         parser = scoreParser(pitch_prob, rhythm_prob)
         rhythms, pitches = parser.parse()
-        print rhythms
-        print pitches
+        
+        r_and_p = zip(rhythms, pitches)
+        with open('hmm_parsed_'+str(i*3+j) + '.txt', 'w') as f:
+            for item in r_and_p:
+                print(str(item[0]) + '\t' + item[1] , file = f)
 
 fpitch.close()
 frhythm.close()
